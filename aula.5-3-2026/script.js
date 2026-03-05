67ff001fed90f14fe4b54e6afbd7fed71a299654
bia0016 <biaevangelistamoreira@gmail.com>
2026-03-05 12:49:32 -0300

    aula de pw

--- aula.5-3-2026/script.js
+++ aula.5-3-2026/script.js
@@ -34,3 +34,38 @@ var num2 = parseInt(window.prompt("valor 2: "))
 var num3 = num1 + num2;
 document.writeln(`A soma de ${num1} + ${num2} = ${num3}`);
 //parseInt 
+
+//estruturas condicionais: if-else, switch-case
+var idade = 10;
+if(idade >= 18){
+    console.log("Maior de idade");
+} else {
+    console.log("Menor de idade");
+}
+var hora = 10;
+if(hora < 12){
+    console.log("bom dia");
+} else if(hora < 18){
+    console.log("boa tarde");
+} else {
+    console.log("boa noite!");
+}
+
+var diaSemana = 3;
+switch (diaSemana){
+    case 1: console.log("domingo"); break;
+    case 2: console.log("segunda"); break;
+    case 3: console.log("terça"); break;
+    default: console.log("escolha uma opção");
+}
+
+//estruturas de repetição: for e while
+for (let i = 0; i < 5; i++){
+    console.log("iteração: " + i);
+}
+
+var i = 5;
+while(i < 5){
+    console.log("iteração: " + i);
+    i++;
+}
